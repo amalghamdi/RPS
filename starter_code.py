6da5d82af64e34e55a39c849e15f386000ca8dc6
amalghamdi <44754439+amalghamdi@users.noreply.github.com>
2018-11-28 21:26:34 +0300

    Keep score

--- starter_code.py
+++ starter_code.py
@@ -34,6 +34,8 @@ class Game:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
+        self.p1.score = 0
+        self.p2.score = 0
 
     def play_round(self):
         move1 = self.p1.move()
@@ -41,14 +43,33 @@ class Game:
         print(f"Player 1: {move1}  Player 2: {move2}")
         self.p1.learn(move1, move2)
         self.p2.learn(move2, move1)
+        if move1 == move2:
+            print("----Tie!!----")
+        elif beats(move1, move2):
+            print("----** Player one wins! **----")
+            self.p1.score += 1
+        else:
+            print("----** Player Two wins! **----")
+            self.p2.score += 1
 
     def play_game(self):
         print("Game start!")
-        for round in range(3):
+        for round in range(1, 4):
             print(f"Round {round}:")
             self.play_round()
+            print("----------------------------")
+        self.result()
         print("Game over!")
 
+    def result(self):
+        print(f'player one scored: {self.p1.score}')
+        print(f'player two scored: {self.p2.score}')
+        if self.p1.score > self.p2.score:
+            print('Player one wis!')
+        elif self.p2.score > self.p1.score:
+            print('Player two wins!')
+        else:
+            print('Tie!')
 # Change the code so it plays a game between two RandomPlayer objects
 
 
